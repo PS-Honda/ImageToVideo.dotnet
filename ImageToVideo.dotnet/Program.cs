1d0740a114d2e4bb946fcad203a24c83c3286a78
PS-Honda <blue.kimsan@gmail.com>
2021-06-25 02:21:51 +0700

    public code

---
+++ ImageToVideo.dotnet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ImageToVideo.dotnet
+{
+    class Program
+    {
+        static ImageConverter fx;
+        static void Main(string[] args)
+        {
+            fx = new ImageConverter();
+            var path = fx.Convert();
+            Console.WriteLine("Video path : ", path);
+            var x = Console.ReadKey();
+        }
+    }
+}
